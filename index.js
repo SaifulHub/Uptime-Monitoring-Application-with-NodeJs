9f4060581963032c149b8b6cf5b73249bb077524
Saif <saifulcode1@gmail.com>
2021-12-10 11:59:51 +0600

    First Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+* Title: Uptime monitoring Application
+* Description: A RESTFULL API to monitor up or down time of user defined links 
+* Written By: Mr. Saif
+* Date: 10/12/2021
+*/
+
+//Dependencies
+const http = require('http')
+
+//App object - Module Scaffolding
+const app = {};
+
+//Configuration
+app.config = {};
