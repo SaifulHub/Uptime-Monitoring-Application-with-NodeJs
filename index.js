7bba544d310844c9448b6a43ef38077f062a6740
Saif <saifulcode1@gmail.com>
2022-01-03 17:32:30 +0600

    index and package.json has been modified.

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 /*
-* Title: Uptime monitoring Application
-* Description: A RESTFULL API to monitor up or down time of user defined links 
-* Written By: Mr. Saif
-* Date: 10/12/2021
-*/
+ * Title: Uptime monitoring Application
+ * Description: A RESTFULL API to monitor up or down time of user defined links
+ * Written By: Mr. Saif
+ * Date: 10/12/2021
+ *
+ */
 
-//Dependencies
-const http = require('http')
+// Dependencies
+const http = require('http');
+const url = require('url');
 
-//App object - Module Scaffolding
+// App object - Module Scaffolding
 const app = {};
+// Configuration
+app.config = {
+    port: 3000,
+};
 
-//Configuration
-app.config = {};
+// creating Server
+app.createServer = () => {
+    const server = http.createServer(app.handleReqRes);
+    server.listen(app.config.port, () => {
+        console.log(`Listening to port ${app.config.port}`);
+    });
+};
+
+// handle request respopnse
+app.handleReqRes = (req, res) => {
+    // Request Handle
+    // getting the url to parse it
+    const parsedUrl = url.parse(req.url, true);
+    console.log(parsedUrl);
+    const path = parsedUrl.pathname;
+    const trimmedPath =
+        // response handle
+        res.end('Hello coders');
+};
+
+// calling the function to start the server
+app.createServer();
